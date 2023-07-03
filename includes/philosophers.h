9d9be020f9a0109c2ccec5abca98e3a4c24a13d9
SolDavid_76 <david.janusz75@gmail.com>
2023-07-03 15:04:24 +0200

    fin de journee pour la salle

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 15:18:59 by djanusz           #+#    #+#             */
-/*   Updated: 2023/06/30 16:57:26 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/07/03 14:43:31 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,17 @@ typedef struct s_philo
 	int				id;
 	pthread_t		thread_id;
 	unsigned long	start;
-	unsigned long	now;
+	unsigned long	now; //may be useless
 	unsigned long	wait;
 	unsigned long	index;
+	int				nb_philo;
 	int				die;
 	int				eat;
 	int				sleep;
 	int				must_eat;
+	pthread_mutex_t	print;
+	pthread_mutex_t	r_fork;
+	pthread_mutex_t	l_fork;
 	t_rules			*rules;
 }					t_philo;
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,32 +6,65 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 15:23:45 by djanusz           #+#    #+#             */
-/*   Updated: 2023/06/30 17:41:57 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/07/03 14:56:50 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+unsigned long	get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+t_philo	*fork_mutex_init(t_philo *philo)
+{
+	pthread_mutex_t	*res;
+	int				i;
+
+	res = malloc(sizeof(pthread_mutex_t) * philo->nb_philo);
+	if (!res)
+		return (write(1, "NOT ENOUGH MEMORY\n", 18), free(philo), NULL);
+	i = -1;
+	while (++i < philo->nb_philo)
+	{
+		pthread_mutex_init(&res[i], NULL);
+		philo[i].l_fork = res[i];
+	}
+	philo[--i].r_fork = res[0];
+	while (--i)
+		philo[i].l_fork = res[i + 1];
+	return (free(res), philo);
+}
+
 t_philo	*philo_init(t_rules *rules)
 {
-	t_philo	*philo;
-	int		i;
+	t_philo			*philo;
+	pthread_mutex_t	print;
+	unsigned long	start;
+	int				i;
 
 	philo = malloc(sizeof(t_philo) * rules->nb_philo);
 	if (!philo)
 		return (write(1, "NOT ENOUGH MEMORY\n", 18), free(rules), NULL);
-	i = 0;
-	while (i < rules->nb_philo)
+	pthread_mutex_init(&print, NULL);
+	i = -1;
+	start = get_time();
+	while (++i < rules->nb_philo)
 	{
 		philo[i].id = i + 1;
+		philo[i].start = start;
+		philo[i].nb_philo = rules->nb_philo;
 		philo[i].die = rules->die;
 		philo[i].eat = rules->eat;
 		philo[i].sleep = rules->sleep;
 		philo[i].must_eat = rules->must_eat;
-		philo[i].rules = rules;
-		i++;
+		philo[i].print = print;
 	}
-	return (philo);
+	return (free(rules), fork_mutex_init(philo));
 }
 
 t_philo	*parsing(char **args)
@@ -60,39 +93,34 @@ t_philo	*parsing(char **args)
 	return (philo_init(res));
 }
 
-unsigned long	get_time(void)
-{
-	struct timeval	time;
-
-	gettimeofday(&time, NULL);
-	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
-}
-
-void	wait(t_philo *philo, unsigned long time, unsigned long tmp)
-{
-	while (42)
-	{
-		philo->now = get_time();
-		if (philo->now - tmp >= time)
-			return ;
-	}
-}
+// void	wait(t_philo *philo, unsigned long time, unsigned long tmp)
+// {
+// 	while (42)
+// 	{
+// 		philo->now = get_time();
+// 		if (philo->now - tmp >= time)
+// 			return ;
+// 	}
+// }
 
 void	*routine(void *arg)
 {
 	t_philo *philo = arg;
 
 	philo->index = 0;
-	philo->start = get_time();
 	philo->now = philo->start;
 	while (42)
 	{
+		pthread_mutex_lock(&philo->print);
 		printf("%lu %d is eating\n", get_time() - philo->start, philo->id);
+		pthread_mutex_unlock(&philo->print);
 		philo->wait = get_time();
 		while (get_time() - philo->wait < (unsigned long)(philo->eat))
 		{
 		}
+		pthread_mutex_lock(&philo->print);
 		printf("%lu %d is sleeping\n", get_time() - philo->start, philo->id);
+		pthread_mutex_unlock(&philo->print);
 		philo->wait = get_time();
 		while (get_time() - philo->wait < (unsigned long)(philo->sleep))
 		{
@@ -109,8 +137,9 @@ int	main(int ac, char **av)
 	philo = parsing(av + 1);
 	if (!philo)
 		return (1);
+	printf("nb_philo = %d, die = %d, eat = %d, sleep = %d, must_eat = %d\n", philo->nb_philo, philo->die, philo->eat, philo->sleep, philo->must_eat);
 	pthread_create(&(philo[0].thread_id), NULL, routine, &philo[0]);
-	printf("nb_philo = %d, die = %d, eat = %d, sleep = %d, must_eat = %d\n", philo->rules->nb_philo, philo->rules->die, philo->rules->eat, philo->rules->sleep, philo->rules->must_eat);
+	pthread_create(&(philo[1].thread_id), NULL, routine, &philo[1]);
 	while (42)
 	{
 	}
